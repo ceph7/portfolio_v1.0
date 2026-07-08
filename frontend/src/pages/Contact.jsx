dac0040449234f7595ea224a2bb2738fdd7aca20
Seraph Cephas ADZIMAH <maccephasadzimah@MacBook-de-Seraph.local>
2026-07-08 17:23:57 +0000

    fichiers des pages contact

---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+function Contact(){
+
+    return (
+
+        <section>
+            <h2>Contactez-moi</h2>
+            <p>Vous pouvez me contacter via le formulaire ci-dessous :</p>
+
+        </section>
+    )
+}
+export default Contact;
