d1135f1b48f5cd3e02465af106100f4acc5eb8ba
Seraph Cephas ADZIMAH <maccephasadzimah@MacBook-de-Seraph.local>
2026-07-08 17:39:49 +0000

    fichier contact

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -2,11 +2,23 @@ function Contact(){
 
     return (
 
-        <section>
+        <section className="contact-page">
             <h2>Contactez-moi</h2>
             <p>Vous pouvez me contacter via le formulaire ci-dessous :</p>
 
+            <form className="contact-form">
+                <label htmlFor="name">Nom :</label>
+                <input type="text" id="name" name="name" placeholder="Votre nom" required />
+
+                <label htmlFor="email">Email :</label>
+                <input type="email" id="email" name="email" placeholder="example@gmail.com" required />
+
+                <label htmlFor="message">Message :</label>
+                <textarea id="message" name="message" placeholder="Votre message" required></textarea>
+
+                <button type="submit" className="btn btn-primary">Envoyer</button>
+            </form>
         </section>
-    )
+    );
 }
 export default Contact;
