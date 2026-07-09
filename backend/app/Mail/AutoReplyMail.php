519edd33f582f63dca3812ef326a8122b09b65bc
Seraph Cephas ADZIMAH <maccephasadzimah@MacBook-de-Seraph.local>
2026-07-09 11:38:26 +0000

    modification et resolution mailing-001

--- backend/app/Mail/AutoReplyMail.php
+++ backend/app/Mail/AutoReplyMail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class AutoReplyMail extends Mailable
+class AutoReplyMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
