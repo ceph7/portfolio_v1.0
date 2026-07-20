a8371c9d9f1ee0ab50b6aada979e46035d32f261
Seraph Cephas ADZIMAH <maccephasadzimah@MacBook-de-Seraph.local>
2026-07-20 22:40:26 +0000

    feature-002

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -47,7 +47,7 @@ function Footer() {
       </div>
 
       <div className="footer-bottom">
-        <p>&copy; 2026 Mon Portfolio. Tous droits réservés.</p>
+        <p>&copy; 2026 Mon Portfolio.Tous droits réservés.</p>
       </div>
       
     </footer>
